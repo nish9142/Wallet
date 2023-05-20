1b5f08671d66cf0b4aafbc3824da2bfac45c522f
Satya Nishanth <satya@Satyas-MacBook-Pro.local>
2023-05-21 01:38:36 +0530

    app.ts changed

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public", "build")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use('/api', walletRoutes);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,"..", "public","build","index.html"));
+    res.sendFile(path.join(__dirname,"..", "public","index.html"));
 });
 
 export default app
