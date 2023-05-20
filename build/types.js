6059203e5c2aec48bd6a597b3ca3a57fde1303e3
Satya Nishanth <satya@Satyas-MacBook-Pro.local>
2023-05-20 23:56:39 +0530

    removed unecessary files

--- build/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransactionType = void 0;
-var TransactionType;
-(function (TransactionType) {
-    TransactionType["CREDIT"] = "CREDIT";
-    TransactionType["DEBIT"] = "DEBIT";
-})(TransactionType = exports.TransactionType || (exports.TransactionType = {}));
-//# sourceMappingURL=types.js.map
